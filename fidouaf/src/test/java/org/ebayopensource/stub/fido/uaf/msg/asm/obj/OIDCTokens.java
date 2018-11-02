1a2bbc310b1894d6a690cc0787dcf959616a4b61
mabr8 <mabr8@PLWRK072.Corp.Internal>
2018-11-02 11:29:44 +0000

    Stub refactoring simplification

--- fidouaf/src/test/java/org/ebayopensource/stub/fido/uaf/msg/asm/obj/OIDCTokens.java
+++
@@ -1,9 +0,0 @@
-package org.ebayopensource.stub.fido.uaf.msg.asm.obj;
-
-public class OIDCTokens {
-    public String access_token; //required DOMString      access_token;
-    public String token_type; //required DOMString      token_type;
-    public String refresh_token; //required DOMString      refresh_token;
-    public int expires_in; //required unsigned short expires_in;
-    public String id_token; //required DOMString      id_token;
-}
